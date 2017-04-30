313fd41db85f29f7a5a3977e5c4f053521d84044
Miguel <miki.mejias85@gmail.com>
2017-04-30 16:34:10 -0400

    Tutorial 2: Cargando archivos estaticos de la carpeta public

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+/*
 app.get('/', (req, res) => {
   res.end('Works!');
 } );
+*/
 
-app.listen(3000);
+app.use(express.static('public'));
+
+app.listen(3000, () =>{
+  console.log('Servidor Iniciado');
+});
